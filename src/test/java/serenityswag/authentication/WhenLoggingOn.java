8623ba05252c748bbfb7274dbc4b677cb167c932
Monica Lara ocaña <73643721+LaraMou@users.noreply.github.com>
2025-03-04 12:12:38 +0100

    serenity-bdd-web-testing Lesson 10 https://www.udemy.com/course/serenity-bdd-web-testing/

---
+++ src/test/java/serenityswag/authentication/WhenLoggingOn.java
@@ -0,0 +1,33 @@
+package serenityswag.authentication;
+
+import net.serenitybdd.annotations.Managed;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SerenityRunner.class)
+public class WhenLoggingOn {
+// Managed anotation  sirve para instanciar el webdriver
+    @Managed
+    WebDriver driver;
+
+    @Test
+    public void usersCanLogOnViaTheHomePage(){
+        driver.get("https://www.saucedemo.com/");
+
+        //como estamos testeando una web. el webdriver permite detectar los css
+        //del form
+        driver.findElement(By.cssSelector("[data-test='username']")).sendKeys("standard_user");
+        driver.findElement(By.cssSelector("[data-test='password']")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("[data-test='login-button']")).click();
+
+        //when login stadard queremos ver los productos ,ejemplo  solo el title
+        assertThat(driver.findElement(By.cssSelector(".title")).getText()).isEqualToIgnoringCase("Products");
+
+
+    }
+}
